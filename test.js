09e48521d4c91661db9825e72498d0f8e461f3a4
LyQing <yjxx_2004@outlook.com>
2022-12-06 13:36:21 +0800

    1

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 console.log(window.navigator.userAgent);
 console.log(window.screen.width);
 console.log(window.screen.height);
+
+let script = document.querySelector('script');
+let attScript = script.attributes;
+console.log(attScript.src);
+
+let h1 = document.querySelector('h1');
+console.log(h1.innerText);
